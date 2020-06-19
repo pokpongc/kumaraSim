0a159c0828d2e6d3815cbbf7a1583d9ae3b61ca1
aminballoon <balloonkogoro@gmail.com>
2020-06-19 16:35:37 +0700

    Add mapping

--- kumara_gazebo/scripts/head_parser.py
+++ kumara_gazebo/scripts/head_parser.py
@@ -17,13 +17,13 @@ primitive_head_z_pub = rospy.Publisher('kumara/neck_z_position_controller/comman
 rospy.init_node('head_parser')
 
 def head_position_callback(pose):
-    q = pose.orientation
+    q = pose.pose.orientation
     rpy = euler_from_quaternion([q.x, q.y, q.z, q.w])
     primitive_head_rx_pub.publish(rpy[0])
     primitive_head_ry_pub.publish(rpy[1])
     primitive_head_rz_pub.publish(rpy[2])
-    primitive_head_x_pub.publish(pose.position.x)
-    primitive_head_z_pub.publish(pose.position.z)
+    primitive_head_x_pub.publish(pose.pose.position.x)
+    primitive_head_z_pub.publish(pose.pose.position.z)
     
 
 if __name__ == '__main__':
